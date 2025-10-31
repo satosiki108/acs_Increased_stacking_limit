e21cdbb2787fd9d279c2628004354795c5c5ab01
Tz_TonZa <satosiki108@gmail.com>
2025-10-31 22:25:10 +0700

    Add files via upload

---
+++ Scripts/MOREMAXSTACK.lua
@@ -0,0 +1,20 @@
+﻿local STACK = GameMain:GetMod("MOREMAXSTACK");
+
+function STACK:OnBeforeInit()
+
+local b,data = CS.XiaWorld.ThingMgr.Instance.m_mapThingDefs:TryGetValue(2);
+
+	for k, v in pairs(data) do
+		if v ~= nil then
+			if v.MaxStack ~= nil then
+				if v.MaxStack ~= 1 then
+					v.MaxStack = 9999
+				end
+			end
+		end 
+	end
+
+CS.XiaWorld.ThingMgr.Instance:GetDef(g_emThingType.Space,"StorageSpace").MaxStack = 9999
+
+end
+
